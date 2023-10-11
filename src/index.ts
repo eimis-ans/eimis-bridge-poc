20b6f2688384c02af8c0bfaa937d4054b5769b17
Pipo Bimbo <heroinedor@gmail.com>
2023-10-11 11:36:00 +0200

    feat: bridge connected to slack

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,57 @@ limitations under the License.
 */
 
 import { AppServiceRegistration, Bridge, Cli, Logger } from 'matrix-appservice-bridge';
+import {IncomingMessage, ServerResponse} from "http";
 
 Logger.configure({ console: "debug" });
 const log = new Logger("index");
 
 let bridge: Bridge;
+
+const http = require("node:http");
+const qs = require("node:querystring"); // we will use this later
+// const requestLib = require("request"); // we will use this later
+
+http.createServer(function(request: IncomingMessage, response: ServerResponse) {
+    console.log(request.method + " " + request.url);
+
+    let body = "";
+    request.on("data", function(chunk: string) {
+
+        type SlackChallenge = { token: string; challenge: string; type: string;}
+        // Validate this value with a custom type guard (extend to your needs)
+        function isSlackChallenge(o: any): o is SlackChallenge {
+            return "token" in o && "challenge" in o
+        }
+
+        const body_content = JSON.parse(chunk);
+        console.log(body_content);
+        if (isSlackChallenge(body_content)) {
+            // do something with now correctly typed object
+            body = body_content.challenge
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.write(body);
+            response.end();
+        } else {
+            // error handling; invalid JSON format
+            body += chunk;
+        }
+    });
+
+    request.on("end", function() {
+        console.log("body=" + body);
+        const params = qs.parse(body);
+        if (params.user_id !== "USLACKBOT") {
+            // const intent = bridge.getIntent("@slack_" + params.user_name + ":matrix.local");
+            const intent = bridge.getIntent("@eimis_admin:matrix.local");
+            intent.sendText("!GJSCiQdFcpnmUmHfOL:matrix.local", "pouet and prout");
+        }
+        response.writeHead(200, {"Content-Type": "application/json"});
+        response.write(JSON.stringify({}));
+        response.end();
+
+    });
+}).listen(9090);  // replace me with your actual port number!
 
 new Cli({
     registrationPath: "eimis-registration.yaml",
@@ -50,7 +96,7 @@ new Cli({
                     log.info("onEphemeralEvent : "+ request.getData());    
                 },
 
-                onAliasQuery: function(alias: string, aliasLocalpart: string){
+                onAliasQuery: function(alias: string){
                     log.info("onAliasQuery : "+ alias);
                 },
 
